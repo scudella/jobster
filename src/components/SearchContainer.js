daf7423ede994a75b203a0878ca8e301ffc6b28f
Eduardo S. Libardi <eslibardi@gmail.com>
2023-02-02 12:35:52 -0300

    remove variable not used in SearchContainer component

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -6,7 +6,7 @@ import { useState, useMemo } from 'react';
 
 const SearchContainer = () => {
   const [localSearch, setLocalSearch] = useState('');
-  const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
+  const { isLoading, searchStatus, searchType, sort, sortOptions } =
     useSelector((store) => store.allJobs);
   const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
   const dispatch = useDispatch();
